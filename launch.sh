7ff769b3ec58bd033f32f0e3d9eca67e99f0c2e3
Kyle Coberly <kyle.coberly@gmail.com>
2025-01-08 01:38:30 -0700

    Fix script

--- launch.sh
+++ launch.sh
@@ -7,7 +7,7 @@ sudo docker run \
 --restart always \
 --publish 3010:3010 \
 --env 'PORT=3010' \
---workdir "/home/node/app" \
---volume nextcloud_aio_mastercontainer:/mnt/docker-aio-config \
---volume /var/www/troop-62-calendar-proxy:/app:ro \
+--workdir '/app' \
+--volume /var/www/troop-62-calendar-proxy:/app \
+--volume /var/run/docker.sock:/var/run/docker.sock:ro \
 $(docker build -q .)
