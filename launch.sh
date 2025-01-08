cb6c5974278dfd2b90b129cfbe59838b8e0000b8
Kyle Coberly <kyle.coberly@gmail.com>
2025-01-08 01:50:43 -0700

    Remove volume

--- launch.sh
+++ launch.sh
@@ -9,6 +9,5 @@ sudo docker run \
 --publish 3010:3010 \
 --env 'PORT=3010' \
 --workdir '/app' \
---volume /var/www/troop-62-calendar-proxy:/app \
 --volume /var/run/docker.sock:/var/run/docker.sock:ro \
 troop-62-calendar-proxy
