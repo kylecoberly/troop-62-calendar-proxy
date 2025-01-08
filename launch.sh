76332a2700656d255aaf791efb258441b786bca4
Kyle Coberly <kyle.coberly@gmail.com>
2025-01-08 01:45:37 -0700

    Update docker build

--- launch.sh
+++ launch.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+sudo docker buildx build -t troop-62-calendar-proxy .
 sudo docker run \
 --init \
 --name troop-62-calendar-proxy \
@@ -10,4 +11,4 @@ sudo docker run \
 --workdir '/app' \
 --volume /var/www/troop-62-calendar-proxy:/app \
 --volume /var/run/docker.sock:/var/run/docker.sock:ro \
-$(docker build -q .)
+troop-62-calendar-proxy
