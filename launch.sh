8d664894d8b3f94ee6b6159b4489cc41f5bd9e03
Kyle Coberly <kyle.coberly@gmail.com>
2025-01-08 01:01:29 -0700

    fix typo

--- launch.sh
+++ launch.sh
@@ -7,7 +7,7 @@ sudo docker run \
 --restart always \
 --publish 3010:3010 \
 --env "PORT=3010" \
---workingDir "/home/node/app" \
+--workdir "/home/node/app" \
 --volume nextcloud_aio_mastercontainer:/mnt/docker-aio-config \
 --volume /var/www/troop-62-calendar-proxy:/home/node/app:ro \
 --entrypoint "sh -c 'npm i && npm start'" \
