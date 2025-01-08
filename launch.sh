cfc3e6f99b0b751615eaefa72bb63fde136810dc
Kyle Coberly <kyle.coberly@gmail.com>
2025-01-08 01:18:44 -0700

    Update launch script

--- launch.sh
+++ launch.sh
@@ -3,11 +3,12 @@
 sudo docker run \
 --init \
 --name troop-62-calendar-proxy \
---user "node" \
+--user 'node' \
 --restart always \
 --publish 3010:3010 \
---env "PORT=3010" \
+--env 'PORT=3010' \
 --workdir "/home/node/app" \
 --volume nextcloud_aio_mastercontainer:/mnt/docker-aio-config \
 --volume /var/www/troop-62-calendar-proxy:/home/node/app:ro \
+--entrypoint 'npm start'
 node:23
