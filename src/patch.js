e8e038025265919191915529073153f7659b25b8
Kyle Coberly <kyle.coberly@gmail.com>
2025-01-08 03:07:04 -0700

    Switch to CRLF

--- src/patch.js
+++ src/patch.js
@@ -6,5 +6,5 @@ module.exports = function patchIcal(source, timezone) {
     .replace(/CREATED:([\dT]+)Z/, (_, $1) => `CREATED:${$1}`)
     .replace(/DTSTART:([\dT]+)Z/, (_, $1) => `DTSTART:${$1}`)
     .replace(/DTEND:([\dT]+)Z/, (_, $1) => `DTEND:${$1}`)
-    .replace(/\$/, "\r");
+    .replace(/\$/, "\r\n");
 };
