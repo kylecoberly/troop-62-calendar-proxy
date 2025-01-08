6e95990dda982859a898c225da176d19baeea436
Kyle Coberly <kyle.coberly@gmail.com>
2025-01-08 00:58:54 -0700

    Add Docker launch command

---
+++ launch.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+sudo docker run \
+--init \
+--name troop-62-calendar-proxy \
+--user "node" \
+--restart always \
+--publish 3010:3010 \
+--env "PORT=3010" \
+--workingDir "/home/node/app" \
+--volume nextcloud_aio_mastercontainer:/mnt/docker-aio-config \
+--volume /var/www/troop-62-calendar-proxy:/home/node/app:ro \
+--entrypoint "sh -c 'npm i && npm start'" \
+node:23
